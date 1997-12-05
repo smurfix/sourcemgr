5c1360b27b4481899c0bfca715d2ead7be0ce0e9
Matthias Urlichs <smurf@play.smurf.noris.de>
1997-12-05 18:31:08 +0100

    redo in p.put eingesetzt Typos (Umlaute 7-bit) in p.h repariert PRCS:STATUS:noris.6

--- bin/p.h
+++ bin/p.h
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 cat <<'END'
-p.add   neue Dateien hinzuf|gen
-p.del   |bersch|ssige Dateien lvschen
-p.diff  Diffs zur letzten Version
-p.edit  editiere die .prj-Datei
-p.get   holt (noris-)Sourcecode aus dem Archiv
-p.import        update externer Sourcen
-p.info  Statusinfo eines Archivs
-p.init  Initiales Installieren von externem Source im Archiv
-p.new   Initiales Installieren von eigenen Sourcen im Archiv
-p.merge Update lokaler Sourcen bei Konflikten beim Checkin
-p.oget  holt Original-Sourcecode aus dem Archiv
-p.put   Speichern von Dnderungen im Archiv
-p.rmcvs Rekursives Lvschen von CVS-spezifischen Subdirectories
-p.update        Update lokaler Sourcen nach externen Dnderungen
+p.add       neue Dateien hinzufügen
+p.cvsimport vollständigen CVS-Baum mit allen (externen) Versionen und
+            (optional) der neuesten internen Version einchecken
+p.del       Überschüssige Dateien löschen
+p.diff      Diffs zur letzten Version
+p.edit      editiere die .prj-Datei
+p.get       holt (noris-)Sourcecode aus dem Archiv
+p.import    update externer Sourcen
+p.info      Statusinfo eines Archivs
+p.init      Initiales Installieren von externem Source im Archiv
+p.new       Initiales Installieren von eigenen Sourcen im Archiv
+p.merge     Update lokaler Sourcen bei Konflikten beim Checkin
+p.oget      holt Original-Sourcecode aus dem Archiv
+p.put       Speichern von Änderungen im Archiv
+p.rmcvs     Rekursives Löschen von CVS-spezifischen Subdirectories
+p.update    Update lokaler Sourcen nach externen Änderungen
 END
