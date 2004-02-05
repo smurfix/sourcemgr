c94635f418a8f47774d2249c268052837cce445f
smurf@kiste.smurf.noris.de <smurf@kiste.smurf.noris.de>
2004-02-05 11:42:41 +0100

    Cset exclude: smurf@smurf.noris.de|ChangeSet|20040128140550|44824

--- bin/b.rcs.pl
+++ bin/b.rcs.pl
@@ -450,7 +450,7 @@ END
 		close(OK);
 		# bk("new"=>"BitKeeper/etc/logging_ok");
 		# bk('-r', ci => '-qG', "-yLogging_OK");
-		# system("bk sfiles -pC | env @DT @AU bk commit -q -yLogging_OK -");
+		# system("bk sfiles -pC | env @DT @AU bk cset -q -yLogging_OK");
 		bk("parent","$ENV{BK_REPOSITORY}/$pn");
 		bk("clone",".","$ENV{BK_REPOSITORY}/$pn");
 	}
@@ -671,7 +671,7 @@ END
 	# $scmt =~ s/\'/\"/g;
 	open(FP,"|-") or do {
 		$scmt =~ s/\001//g;
-		exec("env", @DT,@AU, "bk", "commit", "-q","-y$scmt", "-");
+		exec("env", @DT,@AU, "bk", "cset", "-q","-y$scmt");
 		exit(99);
 	};
 	open(P,"bk sfiles -pC |");
